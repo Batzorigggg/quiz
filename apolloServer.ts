dba503ebf6643f0e0062fd807b8580f9f566293c
BACK-10 <back-10@Erxess-Mac-mini-4.local>
2026-01-19 18:38:35 +0800

    dhcbdhb

--- apolloServer.ts
+++ apolloServer.ts
@@ -1,3 +1,23 @@
+import { quizQueries } from "./src/quiz/graphql/queries.ts";
+import {
+  quizMutationTypeDefs,
+  quizQueryTypeDefs,
+  quizTypeDefs,
+} from "./src/quiz/graphql/schema.ts";
+import {
+  answerMutation,
+  questionMutation,
+} from "./src/quiz/graphql/mutations.ts";
+
 export const typeDefs = `
+    ${quizTypeDefs}
+
+    type Query{
+    ${quizQueryTypeDefs}}
+    type Mutation{${quizMutationTypeDefs}}
 
 `;
+export const resolvers = {
+  Query: { ...quizQueries },
+  Mutation: { ...questionMutation, ...answerMutation },
+};
